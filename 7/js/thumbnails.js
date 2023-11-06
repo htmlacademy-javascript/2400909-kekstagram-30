6ac9ce636a7d978ca88a6ab1c5efda92d268878c
Keks <keks@htmlacademy.ru>
2023-11-06 19:08:44 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/thumbnails.js
@@ -0,0 +1,42 @@
+const thumbnailTemplate = document
+  .querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+
+const createThumbnail = ({url, description, comments, likes, id}) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__img').alt = description;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  //TODO присваем id для каждой картинки
+  thumbnail.dataset.thumbnailId = id;
+
+  return thumbnail;
+};
+
+const renderThumbnails = (pictures, container) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    fragment.append(thumbnail);
+  });
+
+  container.append(fragment);
+};
+
+export { renderThumbnails };
+
+/*
+На основе временных данных для разработки и шаблона #picture создайте DOM-элементы, соответствующие фотографиям, и заполните их данными:
+
+Адрес изображения url подставьте как атрибут src изображения.
+Описание изображения description подставьте в атрибут alt изображения.
+Количество лайков likes выведите в блок .picture__likes.
+Количество комментариев comments выведите в блок .picture__comments.
+Отрисуйте сгенерированные DOM-элементы в блок .pictures. Для вставки элементов используйте DocumentFragment.
+
+Подключите модуль в проект.
+*/
